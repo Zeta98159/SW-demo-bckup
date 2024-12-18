f50de89aee01a7e3945e38a1f66d09dd10bf7195
Andrew Jorgensen <apjorgen@iu.edu>
2024-12-18 09:04:51 -0500

    Final commit

---
+++ sporewood_Demo/Library/main menu/main menu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1); // Main Game Scene
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exiting Game..."); // Log for debugging in the editor
+        Application.Quit(); // Exits the application
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
